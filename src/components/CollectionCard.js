26c2424cfa61f7e0f2b6a5c2bb600bf693e2123d
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2022-02-28 22:09:37 +0000

    Added collectioncard.js

---
+++ src/components/CollectionCard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CollectionCard = () => {
+  return (
+    <div className='collectionCard'>
+        CollectionCard
+        </div>
+  )
+}
+
+export default CollectionCard
