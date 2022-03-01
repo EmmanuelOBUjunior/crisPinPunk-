9f40df3aba6528d93183da6397a88532ecdbea1e
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2022-03-01 18:43:09 +0000

    Adding collection component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import CollectionCard from './components/CollectionCard';
 import Header from './components/Header';
 
 function App() {
   return (
     <div className='app'> 
       <Header/> 
+      <CollectionCard id = {0} name = {'Shrek Punk'} traits = {[{'value': 7}]} image = 'https://lh3.googleusercontent.com/m-Ws730SPYfr1hhPi44UWd923AthhSYnetE8x32yJf1b4GPOTOwdsgK9yXN7naVm7Y884R7e7yTxzMjDGfwpZaRQmfgA4Zo5CXy_jg=w600' />
     </div>
    
   );
